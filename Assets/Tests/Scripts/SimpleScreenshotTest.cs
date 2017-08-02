25328e75dc5411ac0d877550486ec447ed432b33
Tetsuya Mori <monry84@gmail.com>
2017-08-02 14:38:37 +0900

    :sparkles: Supply namespace
    * 冗長だけど、グローバルは汚さない！

--- Assets/Tests/Scripts/SimpleScreenshotTest.cs
+++ Assets/Tests/Scripts/SimpleScreenshotTest.cs
@@ -7,26 +7,30 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
-public class SimpleScreenshotTest {
+namespace SimpleScreenshot.Tests {
 
-    private const string TEST_SCENE_NAME = "Tests/Scenes/CaptureTest";
+    public class SimpleScreenshotTest {
 
-    [UnityTest]
-    public IEnumerator CaptureTest() {
-        SceneManager.LoadScene(TEST_SCENE_NAME);
-        yield return new WaitForEndOfFrame();
-        yield return new WaitForEndOfFrame();
-        // Texture2D 出力を検証
-        SimpleScreenshot.Install(Camera.main).Capture().Subscribe(Assert.IsInstanceOf<Texture2D>);
-        yield return new WaitForEndOfFrame();
-        // ファイル出力を検証
-        string filePath = Path.Combine(System.Environment.GetFolderPath(Environment.SpecialFolder.Personal), string.Format("{0:yyyyMMddHHmmss.fff}.png", DateTime.Now));
-        SimpleScreenshot.Install(Camera.main).CaptureToFile(filePath).Subscribe(
-            (_) => {
-                Assert.IsTrue(File.Exists(filePath));
-                File.Delete(filePath);
-            }
-        );
+        private const string TEST_SCENE_NAME = "Tests/Scenes/CaptureTest";
+
+        [UnityTest]
+        public IEnumerator CaptureTest() {
+            SceneManager.LoadScene(TEST_SCENE_NAME);
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+            // Texture2D 出力を検証
+            SimpleScreenshot.Install(Camera.main).Capture().Subscribe(Assert.IsInstanceOf<Texture2D>);
+            yield return new WaitForEndOfFrame();
+            // ファイル出力を検証
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), string.Format("{0:yyyyMMddHHmmss.fff}.png", DateTime.Now));
+            SimpleScreenshot.Install(Camera.main).CaptureToFile(filePath).Subscribe(
+                (_) => {
+                    Assert.IsTrue(File.Exists(filePath));
+                    File.Delete(filePath);
+                }
+            );
+        }
+
     }
 
 }
